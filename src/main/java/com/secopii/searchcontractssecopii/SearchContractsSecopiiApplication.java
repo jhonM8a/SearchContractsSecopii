ddd9e785d1d4da1a2cf3c9276f8bf2f84eb1ef29
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-09-16 19:56:19 -0500

    first commit

---
+++ src/main/java/com/secopii/searchcontractssecopii/SearchContractsSecopiiApplication.java
@@ -0,0 +1,13 @@
+package com.secopii.searchcontractssecopii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchContractsSecopiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchContractsSecopiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secopii/searchcontractssecopii/SearchContractsSecopiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secopii.searchcontractssecopii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchContractsSecopiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
