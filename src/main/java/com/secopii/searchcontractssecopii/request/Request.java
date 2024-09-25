ef06235161832450372c6fc9dd036c2d1d225c6f
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-09-24 21:07:16 -0500

    add services to search

---
+++ src/main/java/com/secopii/searchcontractssecopii/request/Request.java
@@ -0,0 +1,7 @@
+package com.secopii.searchcontractssecopii.request;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Request {
+}
---
+++ src/main/java/com/secopii/searchcontractssecopii/services/impl/SearchService.java
@@ -0,0 +1,9 @@
+package com.secopii.searchcontractssecopii.services.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+
+
+}
