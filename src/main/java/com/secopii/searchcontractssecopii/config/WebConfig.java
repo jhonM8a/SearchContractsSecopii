3c2786fd2694b3a97630e182bfc22a29f7916a67
Jhon Ochoa <jhon.ochoa@openintl.com>
2024-09-24 21:28:03 -0500

    cambios olvidados

--- src/main/java/com/secopii/searchcontractssecopii/config/WebConfig.java
+++ src/main/java/com/secopii/searchcontractssecopii/config/WebConfig.java
@@ -1,2 +1,18 @@
-package com.secopii.searchcontractssecopii.config;public class WebConfig {
+package com.secopii.searchcontractssecopii.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // O cualquier dominio que necesites permitir
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
--- src/main/java/com/secopii/searchcontractssecopii/repository/model/dao/UserLogin.java
+++ src/main/java/com/secopii/searchcontractssecopii/repository/model/dao/UserLogin.java
@@ -1,2 +1,14 @@
-package com.secopii.searchcontractssecopii.repository.model.dao;public class UserLogin {
+package com.secopii.searchcontractssecopii.repository.model.dao;
+
+
+public class UserLogin extends UserDAO {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
